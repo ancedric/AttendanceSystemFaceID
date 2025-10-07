21df002beb87fc155eb4e146fd9532c520a3d793
ancedric <ancedric55@gmail.com>
2025-10-07 14:38:13 +0200

    Initial commit

---
+++ SmartATSFaceID/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Windows.Threading
+Class MainWindow
+    Private timer As DispatcherTimer
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        setupTimer()
+    End Sub
+
+    Private Sub SetupTimer()
+        timer = New DispatcherTimer With {
+            .Interval = TimeSpan.FromMilliseconds(100) ' Mettre à jour toutes les 100ms
+            }
+        AddHandler timer.Tick, AddressOf Timer_Tick
+        timer.Start()
+    End Sub
+    Private Sub Timer_Tick(sender As Object, e As EventArgs)
+        If progressBar.Value < progressBar.Maximum Then
+            progressBar.Value += 1
+        Else
+            timer.Stop() ' Arrêter le timer une fois terminé
+            Dim login As New loginScreen
+            Me.Close()
+            login.Show()
+        End If
+    End Sub
+End Class
